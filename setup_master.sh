8fc39b9ecebbadd030f3713b67873396095a4ffd
Eric McAlvin <emcalvin@cablevision.com>
2015-03-26 15:19:41 -0400

    Fix ansible keypair generation

--- setup_master.sh
+++ setup_master.sh
@@ -13,14 +13,15 @@ sudo yum install -q -y sshpass
 # ok - permission denied on the ansible folders. need to get access to them... thnking... 
 
 # generate ansible keypair
-if sudo [ -f ~ansible/.ssh/id_rsa ] 
+if [ -f ~ansible/.ssh/id_rsa ] 
 then
     echo "keypair already exists for ansible, doing nothing."
 else
     echo "creating keypair for ansible. DO NOT ENTER A PASSPHRASE."
     sudo -u ansible -s /usr/bin/ssh-keygen -q -t rsa -C ansible@cablevision.com  -f /tmp/id_rsa
-    sudo mv /tmp/id_rsa ~ansible/.ssh
-    sudo mv /tmp/id_rsa.pub ~ansible/.ssh
+    sudo -u ansible -s mkdir -m 700 ~ansible/.ssh
+    sudo mv /tmp/id_rsa ~ansible/.ssh/
+    sudo mv /tmp/id_rsa.pub ~ansible/.ssh/
 fi
 
 # generate keypair for me, which will be added to the authorized_keys for ansible account
