b751b9d6c41bbe0932a20ac85ba2d88e195df53f
Eric McAlvin <emcalvin@cablevision.com>
2015-04-06 10:26:59 -0400

    Add wrapper and cleanup output of all scripts

---
+++ roles/dns-record/files/add_dns_wrapper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]
+then
+  echo "Usage:   $0 START_IP_ADDR END_IP_ADDR FQDN_HOSTNAME"
+  echo "Example: $0 172.16.107.10 172.16.107.250 cvldarch01-54v.cscdev.com"
+  exit 1
+fi
+
+START_IP=$1
+END_IP=$2
+HOSTNAME=$3
+
+IP_TO_USE=`./get_next_available_ip.sh $START_IP $END_IP`
+if echo $IP_TO_USE |grep -q ERROR
+then
+  echo "ERROR: No free IPs available in that range."
+  exit 1
+fi
+
+./dns_add.sh $HOSTNAME $IP_TO_USE
+DNS_ADD_RC=`echo $?`
+
+if [ "$DNS_ADD_RC" -eq 0 ]
+then
+  echo "$IP_TO_USE"
+else
+  echo "ERROR: DNS Add Return Code: $DNS_ADD_RC"
+fi
--- roles/dns-record/files/dns_add.sh
+++ roles/dns-record/files/dns_add.sh
@@ -32,14 +32,14 @@ IP_ADDR=$2
 echo "server $DNS_SERVER
 prereq nxdomain $FQDN.
 update add $FQDN. $TTL IN A $IP_ADDR
-show
 send
 " | /usr/bin/nsupdate -k $KEYFILE
+RC=$?
 
 #REVERSE ENTRY (PTR RECORD) only if A Record was successful
-if [ "$?" -ne 0 ]
+if [ "$RC" -ne 0 ]
 then
-  exit $?
+  exit $RC
 else
   #Reverse the IP Address so it is in IN.ADDR.ARPA format.
   PTR_ADDR_ARRAY=(${IP_ADDR//./ })
@@ -47,7 +47,6 @@ else
   echo "server $DNS_SERVER
   prereq nxdomain $PTR_ADDR
   update add $PTR_ADDR $TTL PTR $FQDN.
-  show
   send
   " | /usr/bin/nsupdate -k $KEYFILE
 fi
