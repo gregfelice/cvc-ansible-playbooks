6a3c44e5e641e54022fd601da4ce233e3c075322
Eric McAlvin <emcalvin@cablevision.com>
2015-05-06 10:39:07 -0400

    First fully working verisign cert request code. Still need to clean it up though.

--- roles/ssl-cert/files/verisign_ssl_api/api_test.py
+++ roles/ssl-cert/files/verisign_ssl_api/api_test.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
 #mess around with the Verisign SSL API
 
-##example to use cert based auth:
-#import requests
-#cert_file_path = "cert.pem"
-#key_file_path = "key.pem"
-#
-#url = "https://example.com/resource"
-#params = {"param_1": "value_1", "param_2": "value_2"}
-#cert = (cert_file_path, key_file_path)
-#r = requests.get(url, params=params, cert=cert, verify=False)
-
 import requests
+from xml.dom import minidom
+
 #Disable the warning for InsecurePlatformWarning when doing REST calls
 requests.packages.urllib3.disable_warnings()
 
-csr = open('wwwapps.cscdev.com-05-05-2015_1514.csr', 'rb')
+csr = open('www4apps.cscdev.com-05-06-2015_1036.csr', 'rb')
 rest_enroll_url = 'https://pilot-certmanager-webservices.websecurity.symantec.com/vswebservices/rest/services/enroll'
 
 rest_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -28,17 +20,34 @@ rest_payload = {
   'certProductType': 'Server',
   'serverType': 'Apache',
   'validityPeriod': '1Y',
-  'signatureAlgorithm' 'sha256WithRSAEncryption',
+  'signatureAlgorithm': 'sha256WithRSAEncryption',
   'csr': csr.read()
 }
 
 
 
-verisign_ssl_cert = r'verisign_ssl_client_cert_api.pem'
+verisign_ssl_client_cert = r'verisign_ssl_client_cert_api.pem'
 session = requests.Session()
-session.cert = (verisign_ssl_cert, verisign_ssl_cert)
+session.cert = (verisign_ssl_client_cert, verisign_ssl_client_cert)
 r = session.post(rest_enroll_url, headers=rest_headers, data=rest_payload)
+print r.status_code
+print r.text
 
-print r.content
+#parse the XML response to get the cert and transactionId
+verisign_response_xml = minidom.parseString(r.text)
+verisign_response_status_code = verisign_response_xml.getElementsByTagName("StatusCode")[0]
+verisign_response_message = verisign_response_xml.getElementsByTagName("Message")[0]
+verisign_response_certificate = verisign_response_xml.getElementsByTagName("Certificate")[0]
+verisign_response_transactionId = verisign_response_xml.getElementsByTagName("Transaction_ID")
+
+print verisign_response_certificate.firstChild.data
+
+#write the cert out to a file
+cert_file = open('www4apps.cscdev.com-05-06-2015_1036.crt', 'w')
+cert_file.write(verisign_response_certificate.firstChild.data)
+
+#close everything
+cert_file.close()
 session.close()
 csr.close()
+
