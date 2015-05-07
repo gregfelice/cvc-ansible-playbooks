c6c063d22ab648a0ad64657527479c8007c89ae7
Eric McAlvin <emcalvin@cablevision.com>
2015-05-07 09:04:21 -0400

    segregate self signed from verisign SSL cert. Removed short name from self signed

--- roles/ssl-cert-selfsigned/README.md
+++ roles/ssl-cert-selfsigned/README.md
@@ -2,13 +2,12 @@
 
 ### This role expects the following variables to be set:
 * fqdn		THIS IS THE WEBSITE FQDN. EXAMPLE: mywebapp.cablevision.com
-* short_name	THIS IS THE SUBJECT ALTERNATE NAME OF THE SSL CERT. Usually the short name of the site. Ex: mywebapp
 * cert_length_years	THIS IS THE LENGTH (IN YEARS) OF THE SSL CERTIFICATE. TYPICALLY 1
 
 ### The output of a successful play is the filename of the SSL certificate PEM file (key + cert).  It gets stored in the variable: ssl_cert_pem_filename
 
 ### To run directly via commandline:
-`ansible-playbook -i hosts_emcalvin_ssl_cert_example roles/ssl-cert/tasks/main.yml --extra-vars "fqdn=cvldarch01-11v.cscdev.com short_name=cvldarch01-11v cert_length_years=2"`
+`ansible-playbook -i hosts_emcalvin_ssl_cert_example roles/ssl-cert-selfsigned/tasks/main.yml --extra-vars "fqdn=cvldarch01-11v.cscdev.com cert_length_years=2"`
 
 ### The inventory file in the above example uses the localhost to run the playbook and looks like this:
 ```
--- roles/ssl-cert-selfsigned/files/generate_ssl_selfsigned.sh
+++ roles/ssl-cert-selfsigned/files/generate_ssl_selfsigned.sh
@@ -3,18 +3,16 @@
 #verify the args
 if [ $# -ne 4 ]
 then
-  echo "Usage:   $0 FQDN ShortName Certificate_Validity_Length_Years location_to_save"
-  echo "Example: $0 mywebapp.cablevision.com mywebapp 1 /home/ansible/certs/"
+  echo "Usage:   $0 FQDN Certificate_Validity_Length_Years location_to_save"
+  echo "Example: $0 mywebapp.cablevision.com 1 /home/ansible/certs/"
   exit 1
 fi
 
 FULL_NAME=$1
-SHORT_NAME=$2
-VALID_YEARS=$3
-CERT_DIR=$4
+VALID_YEARS=$2
+CERT_DIR=$3
 VALID_DAYS=$(($VALID_YEARS*365))
-#ORGANIZATION="Cablevision Systems Corporation"
-ORGANIZATION="Cabelvision Systems Corporation"     #mispelled on purpose for API test. defined in our VICE DEV account at Verisign like this.
+ORGANIZATION="Cablevision Systems Corporation"
 LOCALITY="Bethpage"
 STATE="New York"
 COUNTRY="US"
@@ -27,9 +25,9 @@ TODAY=`date +%m-%d-%Y_%H%M`
 /usr/bin/openssl genrsa -out $CERT_DIR/$FULL_NAME-$TODAY.key 2048 &>/dev/null
 
 #step 2 - generate the CSR
-/usr/bin/openssl req -new -subj "/CN=$FULL_NAME/O=$ORGANIZATION/OU=$OU/C=$COUNTRY/ST=$STATE/L=$LOCALITY/subjectAltName=$SHORT_NAME" -key $CERT_DIR/$FULL_NAME-$TODAY.key -out $CERT_DIR/$FULL_NAME-$TODAY.csr &>/dev/null
+/usr/bin/openssl req -new -subj "/CN=$FULL_NAME/O=$ORGANIZATION/OU=$OU/C=$COUNTRY/ST=$STATE/L=$LOCALITY" -key $CERT_DIR/$FULL_NAME-$TODAY.key -out $CERT_DIR/$FULL_NAME-$TODAY.csr &>/dev/null
 
-#step 3 - generate the self signed cert from the CSR. This can be replaced with Verisign API later.
+#step 3 - generate the self signed cert from the CSR.
 /usr/bin/openssl x509 -req -days $VALID_DAYS -in $CERT_DIR/$FULL_NAME-$TODAY.csr -signkey $CERT_DIR/$FULL_NAME-$TODAY.key -out $CERT_DIR/$FULL_NAME-$TODAY.crt &>/dev/null
 
 #step 4 - put both the key and the cert in the same pem file
